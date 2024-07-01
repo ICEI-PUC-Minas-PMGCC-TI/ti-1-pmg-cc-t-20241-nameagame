1015705d24f9513d4e884f73b538f865dd7eaf4f
DavyKandido <161776341+DaviKandido@users.noreply.github.com>
2024-07-01 11:30:58 -0300

    erro linha 41, idPage

--- codigo/Assets/js/Plataformas.js
+++ codigo/Assets/js/Plataformas.js
@@ -38,7 +38,7 @@ const idPagePlataformas = paramPlataformas.get("id");
    * Apaga do JSON server os objetos
    */
   function DeleteAllPlataformas() {
-    fetch(`${dataURL}/Plataformas`)
+    fetch(`${dataURL}/Plataformas/${idPagePlataformas}`)
       .then(response => response.json())
       .then(data => {
         const deletePromises = data.map(plataforma => {
